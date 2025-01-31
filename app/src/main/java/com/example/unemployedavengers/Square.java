1180de148117d42ccb44ceb9bdd07f0af63d0700
Levi <leviliu@Levis-MacBook-Pro.local>
2025-01-31 15:36:43 -0700

    square extend shape

--- app/src/main/java/com/example/unemployedavengers/Square.java
+++ app/src/main/java/com/example/unemployedavengers/Square.java
@@ -1,4 +1,15 @@
 package com.example.unemployedavengers;
 
-public class Square {
+public class Square extends Shape{
+    private float side;
+
+    public Square(int x, int y) {
+        super(x, y);
+    }
+    public float getSide() {
+        return side;
+    }
+    public double getArea(){
+        return side*side;
+    }
 }

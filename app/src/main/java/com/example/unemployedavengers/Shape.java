82a44846170225c3cbe9837c7c9aa0b03220ccde
Stitches322 <84876526+EthanShi-push@users.noreply.github.com>
2025-01-31 16:04:06 -0700

    Update Shape.java

--- app/src/main/java/com/example/unemployedavengers/Shape.java
+++ app/src/main/java/com/example/unemployedavengers/Shape.java
@@ -1,12 +1,14 @@
 package com.example.unemployedavengers;
 
 public abstract class Shape {
+    String color = "blue";
     protected int x;
     protected int y;
 
-    public Shape(int x, int y) {
+    public Shape(int x, int y,String color) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 
 }

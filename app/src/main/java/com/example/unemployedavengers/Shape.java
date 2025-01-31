e7c81cc50ce50059119f57a513594c1be9e9595e
weikun0309 <122319716+weikun0309@users.noreply.github.com>
2025-01-31 16:14:29 -0700

    halfcircle

--- app/src/main/java/com/example/unemployedavengers/Shape.java
+++ app/src/main/java/com/example/unemployedavengers/Shape.java
@@ -3,7 +3,7 @@ package com.example.unemployedavengers;
 public abstract class Shape {
     protected int x;
     protected int y;
-
+    String color="blue";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
